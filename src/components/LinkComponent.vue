6cf700557303aea16baa320120c273027a4e5169
Alex Gonzalez <galex2003@icloud.com>
2024-03-26 15:35:52 -0400

    first commit

---
+++ src/components/LinkComponent.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <a
+      class="button"
+      :class="{ buttonClicked: clicked, buttonNotClicked: !clicked }"
+      @click="clicked = true"
+      :href="link"
+      target="_blank"
+    >
+      <h1>{{ name }}</h1>
+      <img :src="imageURL" :alt="'Alex\'s ' + name" />
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const clicked = ref(false)
+const props = defineProps({
+  imgURL: {
+    type: String,
+    required: false,
+    default: '@/assets/icons/default.png'
+  },
+  name: {
+    type: String,
+    required: false,
+    default: 'default'
+  },
+  link: {
+    type: String,
+    required: true
+  }
+})
+
+const name = props.name
+const imageURL = props.imgURL
+const link = props.link
+</script>
+
+<style scoped>
+a {
+  height: fill;
+  width: fill;
+}
+
+img {
+  border-radius: 50%;
+  height: 70px;
+  width: 70px;
+  box-shadow: 8px 5px 20px rgba(0, 0, 0, 0.4);
+  transition: transform 0.5s ease;
+  mix-blend-mode: normal;
+}
+
+img:hover {
+  animation: dance 0.5s infinite alternate;
+  opacity: 0.9;
+  mix-blend-mode: soft-light;
+}
+
+@keyframes dance {
+  0% {
+    transform: translateY(0); /* Start position */
+  }
+  50% {
+    transform: translateY(-10px); /* Move up */
+  }
+  100% {
+    transform: translateY(0); /* Return to start position */
+  }
+}
+
+.button {
+  font-family: 'Rubik Doodle Shadow', system-ui;
+  display: flex;
+  font-size: 15px;
+  padding: 10px 20px;
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  align-items: center;
+  margin-bottom: 5px;
+  background: linear-gradient(to right, #0984e3, #a29bfe);
+  transition: transform 0.4s ease;
+  box-shadow:
+    0 4px 16px 0 rgba(0, 0, 0, 0.1),
+    0 6px 20px 0 rgba(0, 0, 0, 0.1);
+}
+
+.button:hover {
+  transform: scale(1.1);
+  filter: brightness(0.9);
+  box-shadow:
+    0 4px 16px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.2);
+}
+
+.button:active:hover {
+  filter: brightness(0.8);
+  transform: perspective(500px) rotateX(10deg) scale(0.9);
+}
+.button img {
+  margin-left: auto;
+}
+
+.container {
+  border-radius: 10px;
+  border-width: 3px;
+  border-color: #2d3436;
+  height: 70px;
+  width: fill;
+}
+
+.buttonClicked {
+  background-color: #a29bfe;
+  background: linear-gradient(to right, #6c5ce7, #a29bfe);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Alex Gonzalez</h1>
+    </div>
+    <div>
+      <ul class="linklist">
+        <li v-for="(link, index) in links" :key="index">
+          <LinkComponent :name="link.name" :imgURL="link.imgURL" :link="link.link" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LinkComponent from '@/components/LinkComponent.vue'
+const links: link[] = [
+  {
+    name: 'Github',
+    link: 'https://github.com/alex-305',
+    imgURL: 'src/assets/icons/github.png'
+  },
+  {
+    name: 'Last.fm',
+    link: 'https://www.last.fm/user/alex_gonz',
+    imgURL: 'src/assets/icons/lastfm.png'
+  },
+  {
+    name: 'Letterboxd',
+    link: 'https://letterboxd.com/alexgonz/films',
+    imgURL: 'src/assets/icons/letterboxd.png'
+  },
+  {
+    name: 'Storygraph',
+    link: 'https://thestorygraph.com/books-read/alex305',
+    imgURL: 'src/assets/icons/storygraph.png'
+  }
+]
+
+interface link {
+  link: string
+  imgURL?: string
+  name?: string
+}
+</script>
+
+<style scoped>
+.title {
+  font-size: 50px;
+  color: black;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.linklist {
+  list-style-type: none;
+  padding: 0px;
+  display: flex;
+  flex-direction: column;
+}
+
+.linklist li {
+  display: block;
+  width: 300px;
+  margin: 0 auto;
+  border-radius: 8px;
+}
+</style>
