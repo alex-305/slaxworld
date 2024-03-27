c4af876f515e34eed8801f4d692772bda6625324
Alex Gonzalez <galex2003@icloud.com>
2024-03-27 00:33:31 -0400

    Added social links

---
+++ src/components/SocialComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a :href="link" target="_blank"> <img :src="imgURL" /> </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  imgURL: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  }
+})
+
+const link = props.link
+const imgURL = props.imgURL
+</script>
+
+<style scoped>
+img {
+  height: 30px;
+  width: 30px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,15 +10,24 @@
         </li>
       </ul>
     </div>
+    <div>
+      <ul class="sociallist">
+        <li v-for="(social, index) in socialLinks" :key="index">
+          <SocialComponent :link="social.link" :imgURL="social.imgURL" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import LinkComponent from '@/components/LinkComponent.vue'
+import SocialComponent from '@/components/SocialComponent.vue'
 import configData from '@/assets/config.json'
 
 let title = configData.title
 let links: link[] = configData.links
+let socialLinks = configData.socials
 
 interface link {
   link: string
@@ -48,10 +57,23 @@ interface link {
   flex-direction: column;
 }
 
+.sociallist {
+  list-style-type: none;
+  padding: 0px;
+  display: flex;
+  flex-direction: row;
+}
+
 .linklist li {
   display: block;
   width: 300px;
   margin: 0 auto;
   border-radius: 8px;
 }
+
+.sociallist li {
+  margin-right: 5px;
+  margin-left: 5px
+}
+
 </style>
